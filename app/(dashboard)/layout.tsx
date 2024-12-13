e679708d8725e54cca03e4bd467e3e14c4bc7c60
Abbo Adamou <abboabakaradamou@gmail.com>
2024-12-12 22:09:43 -0500

    Deployement errors fixed and code cleanup

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,6 @@
 import Sidebar from "@/components/sidebar";
 import Basket from "@/components/basket";
 import Header from "@/components/header";
-import Filter from "@/components/filter";
 
 export default function DashboardLayout({
   children,
@@ -9,11 +8,10 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="antialiased relative flex flex-col md:flex-row max-w-[1920px] mx-auto min-h-screen">
+    <div className="antialiased relative flex flex-col md:flex-row max-w-[1920px] mx-auto min-h-screen w-full">
       <Sidebar />
-      <div className="bg-gray-100/60 flex-1 px-5 py-5 md:ml-20 lg:mr-96">
+      <div className="bg-gray-100/60 flex-1 py-5 md:ml-20 lg:mr-96 md:overflow-hidden">
         <Header />
-        <Filter />
         {children}
       </div>
       <Basket />
--- components/filter.tsx
+++ components/filter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { Button } from "@/components/ui/button";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { cn } from "@/lib/utils";
@@ -27,44 +27,46 @@ const Filter = () => {
     {
       label: "Battery",
       filter: "battery",
-      handleFilter: () => handleFilter("batterie"),
+      handleFilter: () => handleFilter("battery"),
     },
     {
       label: "Pump",
       filter: "pump",
-      handleFilter: () => handleFilter("pompe"),
+      handleFilter: () => handleFilter("pump"),
     },
     {
       label: "Solar Pannel",
       filter: "solar",
-      handleFilter: () => handleFilter("solaire"),
+      handleFilter: () => handleFilter("solar"),
     },
     {
       label: "Inverter",
       filter: "inverter",
-      handleFilter: () => handleFilter("convertisseur"),
+      handleFilter: () => handleFilter("inverter"),
     },
     {
       label: "Pump Kit",
       filter: "pump_kit",
-      handleFilter: () => handleFilter("kitpompe"),
+      handleFilter: () => handleFilter("pump_kit"),
     },
   ];
   return (
-    <div className="wrapper sticky z-10 flex items-center gap-3 overflow-hidden  py-1 lg:py-2">
-      <div className="mr-2 flex items-center gap-1 h-12 rounded-sm border border-base px-5 py-1 pr-5 text-sm lg:font-light">
+    <div className="flex items-center gap-3 overflow-hidden pt-1 md:pt-3">
+      <div className="mr-2 flex items-center gap-1 h-10 md:h-12 rounded-sm border border-base px-5 py-1 pr-5 text-sm lg:font-light">
         <Settings2 className="size-4 stroke-2 stroke-base" /> Filter
       </div>
       <div className="hide-scrollbar flex w-[90%] items-center gap-3 overflow-x-auto py-2 lg:py-5">
-        {filters.map((filter) => (
-          <FilteredButton
-            key={filter.filter}
-            filter={filter.filter}
-            label={filter.label}
-            handleFilter={handleFilter}
-            activeFilter={activeFilter}
-          />
-        ))}
+        <Suspense>
+          {filters.map((filter) => (
+            <FilteredButton
+              key={filter.filter}
+              filter={filter.filter}
+              label={filter.label}
+              handleFilter={handleFilter}
+              activeFilter={activeFilter}
+            />
+          ))}
+        </Suspense>
       </div>
     </div>
   );
@@ -88,7 +90,7 @@ const FilteredButton = ({
       onClick={() => handleFilter(filter)}
       size={"lg"}
       className={cn(
-        "text-primary h-12 px-5 rounded-md font-medium bg-white hover:bg-base hover:rounded-md hover:text-white active:",
+        "text-primary md:h-12 px-5 rounded-md bg-white hover:bg-base hover:rounded-md hover:text-white font-medium",
         {
           "bg-base px-5 rounded-md text-white": activeFilter === filter,
         }
